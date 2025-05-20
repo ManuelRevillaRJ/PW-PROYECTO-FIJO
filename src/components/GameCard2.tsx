300c59d2fda694b39dcc4e5fd44938015caa21af
Manuel Revilla <manuelrevillarj@gmail.com>
2025-05-19 20:15:21 -0500

    se agregó el booton detalle, luego se agregará el modal

--- src/components/GameCard2.tsx
+++ src/components/GameCard2.tsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom"; // Asegúrate de que estás usando react-router-dom
+
 interface GameCardProps {
   image?: string;
   title: string;
   description: string;
+  onDescriptionClick?: (
+    title: string,
+    description: string,
+    image?: string
+  ) => void;
 }
 
 export default function GameCard2(props: GameCardProps) {
@@ -9,9 +16,20 @@ export default function GameCard2(props: GameCardProps) {
     <div className="col">
       <div className="card h-100">
         <img src={props.image} className="card-img-top" alt={props.title} />
-        <div className="card-body">
-          <h5 className="card-title">{props.title}</h5>
-          <p className="card-text">{props.description}</p>
+        <div className="card-body d-flex flex-column justify-content-between">
+          <div>
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.description}</p>
+          </div>
+          <div className="mt-auto">
+            {/* Botón DETALLE */}
+            <Link
+              to={`/detalle/${encodeURIComponent(props.title)}`}
+              className="btn btn-primary w-100 mt-2"
+            >
+              Detalle
+            </Link>
+          </div>
         </div>
       </div>
     </div>
