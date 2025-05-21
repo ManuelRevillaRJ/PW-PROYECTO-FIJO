d4e1be3d9ef3c27e8915f6afcb08a2fe275b6b0c
A15209 <A15209@lab.aca>
2025-05-21 11:21:18 -0500

    Cambio Contraseña Componente y Ruta

--- src/components/ModalJuego.tsx
+++ src/components/ModalJuego.tsx
@@ -1,69 +1,73 @@
-import { Modal, Button } from "react-bootstrap";
-import type { Game } from "../types/Game";
+import { Modal, Button } from "react-bootstrap"
+import type { Game } from "../types/Game"
 
 interface ModalJuegoProps {
-  show: boolean;
-  onHide: () => void;
-  juego: Game;
+    show: boolean
+    onHide: () => void
+    juego: Game
 }
 
 export default function ModalJuego({ show, onHide, juego }: ModalJuegoProps) {
-  const { titulo, description, videoURL, detalleImagenes } = juego;
+    const { titulo, description, videoURL, detalleImagenes } = juego
 
-  return (
-    <Modal show={show} onHide={onHide} centered size="lg" backdrop="static">
-      <Modal.Header closeButton className="bg-dark text-white">
-        <Modal.Title>{titulo}</Modal.Title>
-      </Modal.Header>
+    return (
+        <Modal show={show} onHide={onHide} centered size="lg" backdrop="static">
+            <Modal.Header closeButton className="bg-dark text-white">
+                <Modal.Title>{titulo}</Modal.Title>
+            </Modal.Header>
 
-      <Modal.Body className="bg-dark text-white">
-        <div className="mb-3">
-          <iframe
-            width="100%"
-            height="400"
-            src={videoURL}
-            title={titulo}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>
+            <Modal.Body className="bg-dark text-white">
+                <div className="mb-3">
+                    <iframe
+                        width="100%"
+                        height="400"
+                        src={videoURL}
+                        title={titulo}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                </div>
 
-        <div className="d-flex overflow-auto gap-2 mb-3">
-          {detalleImagenes?.map((img, idx) => (
-            <img
-              key={idx}
-              src={img}
-              alt={`img-${idx}`}
-              className="rounded"
-              width={150}
-            />
-          ))}
-        </div>
+                <div className="d-flex overflow-auto gap-2 mb-3">
+                    {detalleImagenes?.map((img, idx) => (
+                        <img
+                            key={idx}
+                            src={img}
+                            alt={`img-${idx}`}
+                            className="rounded"
+                            width={150}
+                        />
+                    ))}
+                </div>
 
-        <p>{description}</p>
+                <p>{description}</p>
 
-        <div className="mb-3">
-          <p>Calificación:</p>
-          <p>⭐⭐⭐⭐☆</p>
-          
+                <div className="mb-3">
+                    <p>Calificación:</p>
+                    <p>⭐⭐⭐⭐☆</p>
 
-          <div >
-            <div className="row">
-              <Button variant="success" className="col btn btn-success ms-4 me-4">
-                👍 Buen Juego
-              </Button>
-              <Button variant="danger" className=" col me-auto">👎 Mal Juego</Button>
-            </div>
-          </div>
-        </div>
-      </Modal.Body>
+                    <div>
+                        <div className="row">
+                            <Button
+                                variant="success"
+                                className="col btn btn-success ms-4 me-4"
+                            >
+                                👍 Buen Juego
+                            </Button>
+                            <Button variant="danger" className=" col me-auto">
+                                👎 Mal Juego
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </Modal.Body>
 
-      <Modal.Footer className="bg-dark">
-        <Button variant="primary" className="mx-auto p-2">
-          Comprar Ahora
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+            <Modal.Footer className="bg-dark">
+                <Button variant="primary" className="mx-auto p-2">
+                    Comprar Ahora
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
 }
--- src/pages/IniciarSesion.tsx
+++ src/pages/IniciarSesion.tsx
@@ -1,90 +1,96 @@
-import { useState, type FormEvent } from "react";
-import AuthCard from "../components/AuthCard";
-import FormInput from "../components/FormInput";
-import SubmitButton from "../components/Button";
-import FormTitle from "../components/FormTitle";
-import { Link } from "react-router-dom";
-import { revisarAdmin } from "../utils/admins";
+import { useState, type FormEvent } from "react"
+import AuthCard from "../components/AuthCard"
+import FormInput from "../components/FormInput"
+import SubmitButton from "../components/Button"
+import FormTitle from "../components/FormTitle"
+import { Link } from "react-router-dom"
+import { revisarAdmin } from "../utils/admins"
 import { useNavigate } from "react-router-dom"
 
-
 const IniciarSesion = () => {
-  const [nombre, setNombre] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate() 
-  
-  let siExiste: null | boolean = true;
+    const [nombre, setNombre] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
 
-  const handleSubmit = (evt: FormEvent) => {
-    evt.preventDefault();
-    // prueba
-    console.log({ nombre, email, password });
+    let siExiste: null | boolean = true
 
-    // verificacion que existan en base de datos, ahorita prelim
-    siExiste = true;
-    sessionStorage.setItem("user", nombre);
-    if (revisarAdmin (nombre, email, password)){
-      navigate("/games")
+    const handleSubmit = (evt: FormEvent) => {
+        evt.preventDefault()
+        // prueba
+        console.log({ nombre, email, password })
+
+        // verificacion que existan en base de datos, ahorita prelim
+        siExiste = true
+        sessionStorage.setItem("user", nombre)
+        if (revisarAdmin(nombre, email, password)) {
+            navigate("/games")
+        }
     }
 
-    
-  };
+    return (
+        <AuthCard>
+            <form onSubmit={handleSubmit}>
+                <FormTitle
+                    title="Iniciar Sesión en GameStore"
+                    description="Llene los siguientes datos para ingresar:"
+                />
+                <FormInput
+                    label="Nombre"
+                    type="text"
+                    id="nombre"
+                    value={nombre}
+                    onChange={(e) => setNombre(e.currentTarget.value)}
+                />
+                <FormInput
+                    label="Correo Electrónico"
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.currentTarget.value)}
+                />
+                <FormInput
+                    label="Contraseña"
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.currentTarget.value)}
+                />
+                <SubmitButton label="Ingresar" className="mx-auto p-2" />
+            </form>
+            {(() => {
+                if (
+                    !siExiste ||
+                    nombre == "" ||
+                    !email.includes("@") ||
+                    !email.includes(".") ||
+                    password == ""
+                ) {
+                    return (
+                        <button
+                            type="button"
+                            className="btn btn-danger mx-auto p-2"
+                        >
+                            Los datos ingresados son incorrectos
+                        </button>
+                    )
+                }
+            })()}
 
-  return (
-    <AuthCard>
-      <form onSubmit={handleSubmit}>
-        <FormTitle
-          title="Iniciar Sesión en GameStore"
-          description="Llene los siguientes datos para ingresar:"
-        />
-        <FormInput
-          label="Nombre"
-          type="text"
-          id="nombre"
-          value={nombre}
-          onChange={(e) => setNombre(e.currentTarget.value)}
-        />
-        <FormInput
-          label="Correo Electrónico"
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.currentTarget.value)}
-        />
-        <FormInput
-          label="Contraseña"
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.currentTarget.value)}
-        />
-        <SubmitButton label="Ingresar" className="mx-auto p-2" />
-      </form>
-      {(() => {
-        if (!siExiste || nombre == "" || !email.includes("@") || !email.includes(".") || password == "") {
-          return (
-            <button type="button" className="btn btn-danger mx-auto p-2">
-              Los datos ingresados son incorrectos
-            </button>
-          );
-        }
-      })()}
+            <div className="row justify-content-around">
+                <div className="col-4">
+                    <Link to="/crear_cuenta" className="d-block mt-3">
+                        ¿No tienes una cuenta?
+                    </Link>
+                </div>
+                <div className="col-4">
+                    <Link to="/cambio-contra" className="d-block mt-3">
+                        ¿Olvidaste tu contraseña?
+                    </Link>
+                </div>
+            </div>
+        </AuthCard>
+    )
+}
 
-      <div className="row justify-content-around">
-        <div className="col-4">
-          <Link to="/crear_cuenta" className="d-block mt-3">
-            ¿No tienes una cuenta?
-          </Link>
-        </div>
-        <div className="col-4">
-          <Link to="/reset_contraseña" className="d-block mt-3">
-            ¿Olvidaste tu contraseña?
-          </Link>
-        </div>
-      </div>
-    </AuthCard>
-  );
-};
-
-export default IniciarSesion;
+export default IniciarSesion
