cbc4e4c86341070c335deb60672c978a4a50e5a0
A15209 <A15209@lab.aca>
2025-05-28 12:42:59 -0500

    Intento de mail sender

--- src/utils/sesion.ts
+++ src/utils/sesion.ts
@@ -1,8 +1,28 @@
 import type { User } from "../types/types"
+// import SMTP2GOApi from "smtp2go-nodejs"
+
+const SMTP_API_KEY = "api-41D1574A20004588A7559763B372142D"
+const SENDER = "20214689@aloe.ulima.edu.pe"
+// const api = SMTP2GOApi(SMTP_API_KEY)
 
 interface iniciarSesionProps {
     mail: string
     user?: User
+}
+
+export const crearCuenta = ({ mail, user }: iniciarSesionProps) => {
+    if (user) {
+        sessionStorage.setItem("mail", user.correo)
+    } else {
+        sessionStorage.setItem("mail", mail)
+    }
+    // const mailService = api
+    //     .mail()
+    //     .to({ email: mail, name: user?.nombre ?? "Usuario" })
+    //     .from({ email: SENDER })
+    //     .subject("Creacion de Cuenta").html(`<h1>Hello World</h1>
+    // <p>This is a test html email!</p>`)
+    // api.client().consume(mailService)
 }
 
 export const iniciarSesion = ({ mail, user }: iniciarSesionProps) => {
