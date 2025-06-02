9fbe02168525f05eb366d66829f9d2d6dc656cb0
H04308 <H04308@lab.aca>
2025-06-02 12:42:43 -0500

    Mejora de funciones de sesion, comunicacion con el backend

--- src/pages/CrearCuenta.tsx
+++ src/pages/CrearCuenta.tsx
@@ -1,80 +1,96 @@
-import { useState, type FormEvent } from "react"
-import AuthCard from "../components/AuthCard"
-import FormInput from "../components/FormInput"
-import SubmitButton from "../components/Button"
-import FormTitle from "../components/FormTitle"
-import { Link, useNavigate } from "react-router-dom"
-import { iniciarSesion } from "../utils/sesion"
+import { useState, type FormEvent } from "react";
+import AuthCard from "../components/AuthCard";
+import FormInput from "../components/FormInput";
+import SubmitButton from "../components/Button";
+import FormTitle from "../components/FormTitle";
+import { Link, useNavigate } from "react-router-dom";
+import { crearCuenta } from "../utils/sesion";
+import type { User } from "../types/types";
 
 const CrearCuenta = () => {
-    const [nombre, setNombre] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const navigate = useNavigate()
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = (evt: FormEvent) => {
-        evt.preventDefault()
-        // prueba
-        console.log({ nombre, email, password })
+  const handleSubmit = (evt: FormEvent) => {
+    evt.preventDefault();
+    // prueba
 
-        // aca poner que envie correo de confirmacion y que envíe a la pagina de confirmacion
-        iniciarSesion({ mail: email })
-        navigate("/")
-    }
+    const newUser: User = {
+      id: String(Date.now()), // id unico
+      correo: email,
+      password: password,
+      nombre: nombre,
+      token: "",
+      estado: true,
+      permiso: "user", // user | admin
+    };
 
-    return (
-        <AuthCard>
-            <form onSubmit={handleSubmit}>
-                <FormTitle
-                    title="Crear Cuenta en GameStore"
-                    description="Llene los siguientes datos para crear una cuenta:"
-                />
-                <FormInput
-                    label="Nombre"
-                    type="text"
-                    id="nombre"
-                    value={nombre}
-                    onChange={(e) => setNombre(e.currentTarget.value)}
-                />
-                <FormInput
-                    label="Correo Electrónico"
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.currentTarget.value)}
-                />
-                <FormInput
-                    label="Contraseña"
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.currentTarget.value)}
-                />
-                <SubmitButton label="Crear" className="mx-auto p-2" />
-            </form>
-            {(() => {
-                if (
-                    nombre == "" ||
-                    !email.includes("@") ||
-                    !email.includes(".") ||
-                    password == ""
-                ) {
-                    return (
-                        <button
-                            type="button"
-                            className="btn btn-danger mx-auto p-2"
-                        >
-                            Los datos ingresados son incorrectos
-                        </button>
-                    )
-                }
-            })()}
+    crearCuenta({ user: newUser });
+    navigate("/");
 
-            <Link to="/iniciar_sesion" className="d-block mt-3">
-                ¿Ya tienes una cuenta?
-            </Link>
-        </AuthCard>
-    )
-}
+    // try {
+    //   const response = await axios.post("https://your-api-url.com/users", newUser);
+    //   console.log("User created:", response.data);
 
-export default CrearCuenta
+    //   // Redirect after successful account creation
+    //   navigate("/confirmacion");
+    // } catch (error) {
+    //   console.error("Error creating user:", error);
+    // }
+  };
+
+  return (
+    <AuthCard>
+      <form onSubmit={handleSubmit}>
+        <FormTitle
+          title="Crear Cuenta en GameStore"
+          description="Llene los siguientes datos para crear una cuenta:"
+        />
+        <FormInput
+          label="Nombre"
+          type="text"
+          id="nombre"
+          value={nombre}
+          onChange={(e) => setNombre(e.currentTarget.value)}
+        />
+        <FormInput
+          label="Correo Electrónico"
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.currentTarget.value)}
+        />
+        <FormInput
+          label="Contraseña"
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.currentTarget.value)}
+        />
+        <SubmitButton label="Crear" className="mx-auto p-2" />
+      </form>
+      {(() => {
+        if (
+          nombre == "" ||
+          !email.includes("@") ||
+          !email.includes(".") ||
+          password == ""
+        ) {
+          return (
+            <button type="button" className="btn btn-danger mx-auto p-2">
+              Los datos ingresados son incorrectos
+            </button>
+          );
+        }
+      })()}
+
+      <Link to="/iniciar_sesion" className="d-block mt-3">
+        ¿Ya tienes una cuenta?
+      </Link>
+    </AuthCard>
+  );
+};
+
+export default CrearCuenta;
--- src/utils/sesion.ts
+++ src/utils/sesion.ts
@@ -1,38 +1,23 @@
-import type { User } from "../types/types"
-// import SMTP2GOApi from "smtp2go-nodejs"
-
-const SMTP_API_KEY = "api-41D1574A20004588A7559763B372142D"
-const SENDER = "20214689@aloe.ulima.edu.pe"
-// const api = SMTP2GOApi(SMTP_API_KEY)
+import type { User } from "../types/types";
 
 interface iniciarSesionProps {
-    mail: string
-    user?: User
+  user: User;
 }
 
-export const crearCuenta = ({ mail, user }: iniciarSesionProps) => {
-    if (user) {
-        sessionStorage.setItem("mail", user.correo)
-    } else {
-        sessionStorage.setItem("mail", mail)
-    }
-    // const mailService = api
-    //     .mail()
-    //     .to({ email: mail, name: user?.nombre ?? "Usuario" })
-    //     .from({ email: SENDER })
-    //     .subject("Creacion de Cuenta").html(`<h1>Hello World</h1>
-    // <p>This is a test html email!</p>`)
-    // api.client().consume(mailService)
-}
+export const crearCuenta = async ({ user }: iniciarSesionProps) => {
+  sessionStorage.setItem("mail", user.correo);
+  const res = await fetch("http://localhost:3000/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(user),
+  });
+  console.log(res);
+};
 
-export const iniciarSesion = ({ mail, user }: iniciarSesionProps) => {
-    if (user) {
-        sessionStorage.setItem("mail", user.correo)
-    } else {
-        sessionStorage.setItem("mail", mail)
-    }
-}
+export const iniciarSesion = ({ user }: iniciarSesionProps) => {
+  sessionStorage.setItem("mail", user.correo);
+};
 
 export const cerrarSesion = () => {
-    sessionStorage.removeItem("mail")
-}
+  sessionStorage.removeItem("mail");
+};
