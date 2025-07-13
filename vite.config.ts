6d00af52969a8a14411a74047d07cf6b4fd7de02
Manuel Revilla <manuelrevillarj@gmail.com>
2025-07-13 17:27:00 -0500

    lol3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/PW-PROYECTO-FIJO/",
+  base: "/PW_G2_F/",
   server: {
     // port: 3000,
     // proxy: {
