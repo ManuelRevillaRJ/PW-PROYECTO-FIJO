29170cb3ed925ac6f540f31b85b177e02d04fa64
Manuel Revilla <manuelrevillarj@gmail.com>
2025-07-13 23:00:27 -0500

    ya corre

--- src/secret.tsx
+++ src/secret.tsx
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3000"
-export const API_URL2 = "https://pw-proyecto-fijo-backend.onrender.com"
+export const API_URL2 = "http://localhost:3000"
+export const API_URL = "https://pw-proyecto-fijo-backend.onrender.com"
